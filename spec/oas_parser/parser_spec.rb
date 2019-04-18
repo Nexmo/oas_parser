ac8b7dcaec319d57b21e21df78281aa2e4a5e621
Michael Heap <m@michaelheap.com>
2019-04-18 09:24:36 +0100

    Fix JSON pointer bug in local reference resolution

--- spec/oas_parser/parser_spec.rb
+++ spec/oas_parser/parser_spec.rb
@@ -4,6 +4,12 @@ RSpec.describe OasParser::Parser do
     expect(definition.class).to eq(Hash)
   end
 
+  it 'follows JSON pointers in external definitions' do
+    definition = OasParser::Parser.resolve('spec/fixtures/petstore-remote-reference.yml')
+    expect(definition.dig('paths', '/pets', 'get', 'responses', '200',
+                          'content', 'application/json', 'schema', 'items', 'allOf').map {|h| h['properties'].keys }.flatten).to eq (['name', 'tag', 'id'])
+  end
+
   it 'parses references from a definition file with external data' do
     definition = OasParser::Parser.resolve('spec/fixtures/petstore-remote-reference.yml')
 
